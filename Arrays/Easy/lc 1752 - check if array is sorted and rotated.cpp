019d9abddb533000e37571f9ec87595abe1e2b67
devesh1808 <devesh1808@gmail.com>
2025-01-30 22:48:28 +0530

    check if array is sorted and rotated

---
+++ Arrays/Easy/lc 1752 - check if array is sorted and rotated.cpp
@@ -0,0 +1,13 @@
+bool check(vector<int> &nums)
+{
+    int n = nums.size();
+    int cnt = 0;
+    for (int i = 1; i < n; i++)
+    {
+        if (nums[i] < nums[i - 1])
+            cnt++;
+    }
+    if ((nums[n - 1] > nums[0] && cnt > 0) || cnt > 1)
+        return false;
+    return true;
+}
