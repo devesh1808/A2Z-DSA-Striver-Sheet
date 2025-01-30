89d58ac78b0cde1ebe2035dbcc2e4b88d8c6d44b
devesh1808 <devesh1808@gmail.com>
2025-01-30 22:51:47 +0530

    using 2 pointers

---
+++ Arrays/Easy/lc 26 - remove duplicates from sorted array.cpp
@@ -0,0 +1,16 @@
+int removeDuplicates(vector<int> &nums)
+{
+    int n = nums.size();
+    int i = 0, j = 0;
+    while (j < n)
+    {
+        if (nums[i] == nums[j])
+            j++;
+        else
+        {
+            ++i;
+            nums[i] = nums[j];
+        }
+    }
+    return i + 1;
+}
