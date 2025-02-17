9c84eea19bdcf60336b79d9d2b55d4541da71147
devesh1808 <devesh1808@gmail.com>
2025-02-17 23:09:45 +0530

    iterative approach

--- Binary Tree/lc 144 - preorder traversal.cpp
+++ Binary Tree/lc 144 - preorder traversal.cpp
@@ -1,20 +1,50 @@
 // recursive approach
 
+// class Solution
+// {
+// public:
+//     void func(TreeNode *root, vector<int> &ans)
+//     {
+//         if (root == NULL)
+//             return;
+//         ans.push_back(root->val);
+//         func(root->left, ans);
+//         func(root->right, ans);
+//     }
+//     vector<int> preorderTraversal(TreeNode *root)
+//     {
+//         vector<int> ans;
+//         func(root, ans);
+//         return ans;
+//     }
+// };
+
+
+
+// iterative approach
+
 class Solution
 {
 public:
-    void func(TreeNode *root, vector<int> &ans)
-    {
-        if (root == NULL)
-            return;
-        ans.push_back(root->val);
-        func(root->left, ans);
-        func(root->right, ans);
-    }
     vector<int> preorderTraversal(TreeNode *root)
     {
         vector<int> ans;
-        func(root, ans);
+        if (root == NULL)
+            return ans;
+        stack<TreeNode *> st;
+        TreeNode *curr = root;
+        while (!st.empty() || curr != NULL)
+        {
+            while (curr != NULL)
+            {
+                ans.push_back(curr->val);
+                st.push(curr);
+                curr = curr->left;
+            }
+            curr = st.top();
+            st.pop();
+            curr = curr->right;
+        }
         return ans;
     }
 };
