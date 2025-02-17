df015817003e90db45366aef9dbcd3781727c7a5
devesh1808 <devesh1808@gmail.com>
2025-02-17 23:03:35 +0530

    iterative approach

--- Binary Tree/lc 94 - inorder traversal.cpp
+++ Binary Tree/lc 94 - inorder traversal.cpp
@@ -1,19 +1,49 @@
-//recursive solution
+// recursive solution
+//  class Solution
+//  {
+//  public:
+//      void func(TreeNode *root, vector<int> &ans)
+//      {
+//          if (root == NULL)
+//              return;
+//          func(root->left, ans);
+//          ans.push_back(root->val);
+//          func(root->right, ans);
+//      }
+//      vector<int> inorderTraversal(TreeNode *root)
+//      {
+//          vector<int> ans;
+//          func(root, ans);
+//          return ans;
+//      }
+//  };
+
+
+
+// iterative solution
+
 class Solution
 {
 public:
-    void func(TreeNode *root, vector<int> &ans)
-    {
-        if (root == NULL)
-            return;
-        func(root->left, ans);
-        ans.push_back(root->val);
-        func(root->right, ans);
-    }
     vector<int> inorderTraversal(TreeNode *root)
     {
+        if (root == NULL)
+            return {};
         vector<int> ans;
-        func(root, ans);
+        stack<TreeNode *> st;
+        TreeNode *curr = root;
+        while (!st.empty() || curr != NULL)
+        {
+            while (curr != NULL)
+            {
+                st.push(curr);
+                curr = curr->left;
+            }
+            curr = st.top();
+            st.pop();
+            ans.push_back(curr->val);
+            curr = curr->right;
+        }
         return ans;
     }
 };
