dbe4b105d23b3276f57ead332312cc5bb19aed39
devesh1808 <devesh1808@gmail.com>
2025-02-18 23:33:39 +0530

    iterative solution (reverse postorder)

--- Binary Tree/lc 145 - postorder traversal.cpp
+++ Binary Tree/lc 145 - postorder traversal.cpp
@@ -1,20 +1,52 @@
 // recursive solution
 
+// class Solution
+// {
+// public:
+//     void func(TreeNode *root, vector<int> &ans)
+//     {
+//         if (root == NULL)
+//             return;
+//         func(root->left, ans);
+//         func(root->right, ans);
+//         ans.push_back(root->val);
+//     }
+//     vector<int> postorderTraversal(TreeNode *root)
+//     {
+//         vector<int> ans;
+//         func(root, ans);
+//         return ans;
+//     }
+// };
+
+
+
+
+// itetative solution
+
 class Solution
 {
 public:
-    void func(TreeNode *root, vector<int> &ans)
-    {
-        if (root == NULL)
-            return;
-        func(root->left, ans);
-        func(root->right, ans);
-        ans.push_back(root->val);
-    }
     vector<int> postorderTraversal(TreeNode *root)
     {
-        vector<int> ans;
-        func(root, ans);
-        return ans;
+        vector<int> v;
+        if (root == NULL)
+            return v;
+        stack<TreeNode *> st;
+        TreeNode *c = root;
+        while (c != NULL || !st.empty())
+        {
+            while (c != NULL)
+            {
+                v.push_back(c->val);
+                st.push(c);
+                c = c->right;
+            }
+            c = st.top();
+            st.pop();
+            c = c->left;
+        }
+        reverse(v.begin(), v.end());
+        return v;
     }
 };
